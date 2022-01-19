5ca867ad6d8cc4465a196cb124af8cd31b2496ac
alexcasey <davidalexcasey@gmail.com>
2022-01-19 15:06:49 -0700

    +User +AccountController +authorize/authenticate

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string? DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(.*[&._-]).{9,18}$", ErrorMessage = "Invalid password")]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,8 @@ using Application.Activities;
 using API.Extensions;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -33,11 +35,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddFluentValidation(_config => 
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .AddFluentValidation(_config => 
             {
                 _config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +62,8 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
-            // app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
